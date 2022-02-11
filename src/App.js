0b36547cee8262ea1b7a3de0bd16a217864d42e5
Jun Ki Kwon <pkwon35@gmail.com>
2022-02-11 16:37:22 +0900

    deploy note app

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import "./index.css"
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+
+export default function App() {
+    const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || [])
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+
+    
+    React.useEffect(()=>{
+        localStorage.setItem("notes",JSON.stringify(notes))
+    },[notes])
+
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+    
+    function updateNote(text) {
+        setNotes(oldNotes =>{
+            const newdArray = []
+            for (let i=0; i< oldNotes.length;i++){
+                const oldNote=oldNotes[i]
+                if(oldNote.id===currentNoteId){
+                    newdArray.unshift({...oldNote, body: text })
+                }else{
+                    newdArray.push(oldNote)
+                }
+            }
+            return newdArray
+        })
+
+    }
+    
+    function deleteNote(event,noteId){
+        event.stopPropagation()
+        setNotes(oldNotes =>{
+            const newdArray = []
+            oldNotes.forEach(oldNote => {
+              if (oldNote.id!==noteId)  {
+                newdArray.push(oldNote)
+              }
+            })
+
+            return newdArray
+        })
+    }
+
+
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+
+
+
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
+}
